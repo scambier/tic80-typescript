6a39f19214d996cd0630cced6e472482ff1db57c
Simon Cambier <cambier.simon@gmail.com>
2019-01-17 12:51:44 +0100

    Fixed some return types

--- tocopy/tic.d.ts
+++ tocopy/tic.d.ts
@@ -1,6 +1,6 @@
-declare function btn(id: number): void;
+declare function btn(id: number): boolean;
 
-declare function btnp(id: number, hold?: number, period?: number): void;
+declare function btnp(id: number, hold?: number, period?: number): boolean;
 
 declare function clip(x: number, y: number, w: number, h: number): void;
 
@@ -12,7 +12,7 @@ declare function circb(x: number, y: number, r: number, color: number): void;
 
 declare function exit(): void;
 
-declare function font(text: string, x?: number, y?: number, colorkey?: number, w?: number, h?: number, fixed?: boolean, scale?: number): void; // Not in the wiki
+declare function font(text: string, x?: number, y?: number, colorkey?: number, w?: number, h?: number, fixed?: boolean, scale?: number): number;
 
 declare function key(code: number): boolean
 
@@ -36,7 +36,7 @@ declare function music(track?: number, frame?: number, loop?: boolean): void;
 
 declare function peek(addr: number): number;
 
-declare function peek4(addr: number): number; // Not in the wiki
+declare function peek4(addr: number): number;
 
 declare function pix(x: number, y: number, color?: number): number;
 
@@ -44,26 +44,26 @@ declare function pmem(index: number, val?: number): number | void;
 
 declare function poke(addr: number, val: number): void;
 
-declare function poke4(addr: number, val: number): void; // Not in the wiki
+declare function poke4(addr: number, val: number): void;
 
 declare function print(str: string, x?: number, y?: number, color?: number, fixed?: boolean, scale?: number): void;
 
-declare function rect(x: number, y: number, w: number, h: number, color: number);
+declare function rect(x: number, y: number, w: number, h: number, color: number): void;
 
-declare function rectb(x: number, y: number, w: number, h: number, color: number);
+declare function rectb(x: number, y: number, w: number, h: number, color: number): void;
 
 declare function reset(): void
 
-declare function sfx(id: number, note?: number | string, duration?: number, channel?: number, volume?: number, speed?: number);
+declare function sfx(id: number, note?: number | string, duration?: number, channel?: number, volume?: number, speed?: number): void;
 
-declare function spr(id: number, x: number, y: number, colorkey?: number, scale?: number, flip?: number | boolean, rotate?: number, w?: number, h?: number);
+declare function spr(id: number, x: number, y: number, colorkey?: number, scale?: number, flip?: number | boolean, rotate?: number, w?: number, h?: number): void;
 
-declare function sync();
+declare function sync(): void;
 
-declare function textri(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, u1: number, v1: number, u2: number, v2: number, u3: number, v3: number, use_map?: boolean, colorkey?: number);
+declare function textri(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, u1: number, v1: number, u2: number, v2: number, u3: number, v3: number, use_map?: boolean, colorkey?: number): void;
 
 declare function time(): number;
 
 declare function trace(msg: any, color?: number): void;
 
-declare function tri(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, color: number);
+declare function tri(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, color: number): void;
